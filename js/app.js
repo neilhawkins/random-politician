5cf5d734d5f3c66508a00a5bc66e37e718fbbba9
neilhawkins <neilrhawkins@gmail.com>
2014-05-14 14:25:52 +0100

    add party colours, last time spoke in parliament

--- js/app.js
+++ js/app.js
@@ -4,13 +4,27 @@
 
 var RandoMP = {
 
-    getMPs: "http://www.theyworkforyou.com/api/getMPs?key=GEGMS7CVRzUFFvoPV2DbcscS&output=js&callback=?",
+    MPs: null,
+    hansard: null,
 
-    getHansard: function(MP_id) {
-        return "http://www.theyworkforyou.com/api/getHansard?key=GEGMS7CVRzUFFvoPV2DbcscS&output=js&callback=?&person=" + MP_id
+    colours: {
+
+         "Conservative":"#0087DC",
+         "Labour":"#DC241f",
+         "Liberal Democrat":"#FDBB30",
+         "DUP":"#D46A4C",
+         "Scottish National Party":"#FFFF00",
+         "Sinn Fein": "#008800",
+         "Independent":"#DDDDDD",
+         "Plaid Cymru":"#008142",
+         "Social Democratic and Labour Party":"#99FF66",
+         "Alliance":"#FFD700",
+         "Green":"#6AB023",
+         "Respect":"#FF3300",
+         "Speaker":"white"
     },
 
-    MPs: null,
+    getMPs: "http://www.theyworkforyou.com/api/getMPs?key=GEGMS7CVRzUFFvoPV2DbcscS&output=js&callback=?",
 
     init: function(request) {
         this.bind_events();
@@ -19,15 +33,13 @@ var RandoMP = {
 
     bind_events: function() {
         var self = this;
-        $('.btn').on('click', function() {
+        $('.mp').on('click', function() {
+            $('#last-words').html("");
             self.pick_MP();
         });
-
     },
 
     make_request: function(request_url) {
-
-        var self = this;
 
         var response_data = JSON.parse(localStorage.getItem('MPs'));
         this.MPs = response_data;
@@ -39,17 +51,35 @@ var RandoMP = {
                 self.pick_MP();
             });
         } else
-
             this.pick_MP();
 
+    },
+
+    lastWords: function() {
+        var date = this.hansard.rows[0].hdate;
+        date = date.replace(/(\d+)-(\d+)-(\d+)/, '$3/$2/$1');
+        $('#date-spoken').html("<p>Last spoke in parliament on : " + date + "</p>");
+        $('#last-words').html(this.hansard.rows[0].body);
+    },
+
+    getHansard:  function(id) {
+        var self = this; 
+        $.getJSON("http://www.theyworkforyou.com/api/"+
+                "getDebates?key=GEGMS7CVRzUFFvoPV2DbcscS&person=" + id +
+                "&type=commons&page=1&num=1&output=js&callback=?").done(function(data){
+                    self.hansard = data;
+                    self.lastWords();
+                });
     },
 
     pick_MP: function() {
         var random_id = Math.floor(Math.random() * 648);
         var chosen_one = this.MPs[random_id];
-
-        $('#chosen_one h1').text(chosen_one.name + " MP");
-        //$('#chosen_one .last_words').text(app.response_data.rows[0].body)
+        var MP_id = chosen_one.person_id;
+        var party = chosen_one.party;
+        this.MP_id = MP_id;
+        $('#mp-name').text(chosen_one.name + " MP").css('color', this.colours[party]);      
+        RandoMP.getHansard(MP_id);
     }
 }
 
